8b9ab9ba161bb4abdc84b47992763bd41c59efd1
Gabriel <gabrielstrzal@gmail.com>
2020-09-05 13:31:11 -0400

    initial libgdx setup

---
+++ desktop/src/com/strzal/space/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.strzal.space.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.strzal.space.SpaceCatGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceCatGame(), config);
+	}
+}
