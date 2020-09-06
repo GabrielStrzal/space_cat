cbb07626c6a1f2c8bffd086c2c410b161e152176
Gabriel <gabrielstrzal@gmail.com>
2020-09-06 13:56:50 -0400

    changing the version of Lwjgl to 3

--- desktop/src/com/strzal/space/desktop/DesktopLauncher.java
+++ desktop/src/com/strzal/space/desktop/DesktopLauncher.java
@@ -1,12 +1,15 @@
 package com.strzal.space.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.strzal.space.SpaceCatGame;
+import com.strzal.space.config.GameConfig;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SpaceCatGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration ();
+		config.setWindowedMode( (int) GameConfig.SCREEN_WIDTH, (int)GameConfig.SCREEN_HEIGHT);
+		new Lwjgl3Application(new SpaceCatGame(), config);
+
 	}
 }
